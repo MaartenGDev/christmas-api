0ec50f060d0a22fc18943a3cf84c872f998fc263
MaartenGDev <maarten.grootoonk@gmail.com>
2018-01-25 20:41:00 +0100

    Remove unnecessary comments

--- routes/api.php
+++ routes/api.php
@@ -2,17 +2,6 @@
 
 use Illuminate\Http\Request;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
 Route::group(['prefix' => 'v1', 'as' => 'api.', 'namespace' => 'Api\\'], function () {
     Route::resource('gifts', 'UserGiftController');
     Route::get('gift-reservations', 'GiftReservationController@index');
